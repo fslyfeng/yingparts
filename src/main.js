7ce5859f49de01d0611d481b789cd5a229ab7508
netren <fslyfeng@gmail.com>
2020-12-19 23:53:11 +0800

    引入 rem reset.css

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
+import "./assets/rem";
+import "./assets/reset.css";
 import Vue from "vue";
 import App from "./App.vue";
 
 import { Button } from "vant";
-import router from './router'
+import router from "./router";
 Vue.use(Button);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
-
-
